ac4eb5c1a9ef3bada0c1d6cc8597c137e57010f1
Jareth Holt <jarhol@raysearchlabs.com>
2024-10-18 13:08:45 +0200

    Use extracted controls from bar chart controlPanel
    In the Control Panel for the Bar chart, import and use the two functions
    extracted into Timeseries/controls.ts.

--- superset-frontend/plugins/plugin-chart-echarts/src/Timeseries/Regular/Bar/controlPanel.tsx
+++ superset-frontend/plugins/plugin-chart-echarts/src/Timeseries/Regular/Bar/controlPanel.tsx
@@ -19,15 +19,9 @@
 import { t } from '@superset-ui/core';
 import {
   ControlPanelConfig,
-  ControlPanelsContainerProps,
-  ControlSetRow,
-  ControlStateMapping,
   ControlSubSectionHeader,
-  D3_TIME_FORMAT_DOCS,
-  formatSelectOptions,
   getStandardizedControls,
   sections,
-  sharedControls,
 } from '@superset-ui/chart-controls';
 import {
   legendSection,
@@ -37,218 +31,14 @@ import {
   showValueSection,
   truncateXAxis,
   xAxisBounds,
-  xAxisLabelRotation,
 } from '../../../controls';
 
 import { OrientationType } from '../../types';
-import {
-  DEFAULT_FORM_DATA,
-  TIME_SERIES_DESCRIPTION_TEXT,
-} from '../../constants';
+import { DEFAULT_FORM_DATA } from '../../constants';
 
-const {
-  logAxis,
-  minorSplitLine,
-  truncateYAxis,
-  yAxisBounds,
-  zoomable,
-  orientation,
-} = DEFAULT_FORM_DATA;
+import { createAxisTitleControl, createAxisControl } from '../../controls';
 
-function createAxisTitleControl(axis: 'x' | 'y'): ControlSetRow[] {
-  const isXAxis = axis === 'x';
-  const isVertical = (controls: ControlStateMapping) =>
-    Boolean(controls?.orientation.value === OrientationType.Vertical);
-  const isHorizontal = (controls: ControlStateMapping) =>
-    Boolean(controls?.orientation.value === OrientationType.Horizontal);
-  return [
-    [
-      {
-        name: 'x_axis_title',
-        config: {
-          type: 'TextControl',
-          label: t('Axis Title'),
-          renderTrigger: true,
-          default: '',
-          description: t('Changing this control takes effect instantly'),
-          visibility: ({ controls }: ControlPanelsContainerProps) =>
-            isXAxis ? isVertical(controls) : isHorizontal(controls),
-        },
-      },
-    ],
-    [
-      {
-        name: 'x_axis_title_margin',
-        config: {
-          type: 'SelectControl',
-          freeForm: true,
-          clearable: true,
-          label: t('AXIS TITLE MARGIN'),
-          renderTrigger: true,
-          default: sections.TITLE_MARGIN_OPTIONS[0],
-          choices: formatSelectOptions(sections.TITLE_MARGIN_OPTIONS),
-          description: t('Changing this control takes effect instantly'),
-          visibility: ({ controls }: ControlPanelsContainerProps) =>
-            isXAxis ? isVertical(controls) : isHorizontal(controls),
-        },
-      },
-    ],
-    [
-      {
-        name: 'y_axis_title',
-        config: {
-          type: 'TextControl',
-          label: t('Axis Title'),
-          renderTrigger: true,
-          default: '',
-          description: t('Changing this control takes effect instantly'),
-          visibility: ({ controls }: ControlPanelsContainerProps) =>
-            isXAxis ? isHorizontal(controls) : isVertical(controls),
-        },
-      },
-    ],
-    [
-      {
-        name: 'y_axis_title_margin',
-        config: {
-          type: 'SelectControl',
-          freeForm: true,
-          clearable: true,
-          label: t('AXIS TITLE MARGIN'),
-          renderTrigger: true,
-          default: sections.TITLE_MARGIN_OPTIONS[0],
-          choices: formatSelectOptions(sections.TITLE_MARGIN_OPTIONS),
-          description: t('Changing this control takes effect instantly'),
-          visibility: ({ controls }: ControlPanelsContainerProps) =>
-            isXAxis ? isHorizontal(controls) : isVertical(controls),
-        },
-      },
-    ],
-    [
-      {
-        name: 'y_axis_title_position',
-        config: {
-          type: 'SelectControl',
-          freeForm: true,
-          clearable: false,
-          label: t('AXIS TITLE POSITION'),
-          renderTrigger: true,
-          default: sections.TITLE_POSITION_OPTIONS[0][0],
-          choices: sections.TITLE_POSITION_OPTIONS,
-          description: t('Changing this control takes effect instantly'),
-          visibility: ({ controls }: ControlPanelsContainerProps) =>
-            isXAxis ? isHorizontal(controls) : isVertical(controls),
-        },
-      },
-    ],
-  ];
-}
-
-function createAxisControl(axis: 'x' | 'y'): ControlSetRow[] {
-  const isXAxis = axis === 'x';
-  const isVertical = (controls: ControlStateMapping) =>
-    Boolean(controls?.orientation.value === OrientationType.Vertical);
-  const isHorizontal = (controls: ControlStateMapping) =>
-    Boolean(controls?.orientation.value === OrientationType.Horizontal);
-  return [
-    [
-      {
-        name: 'x_axis_time_format',
-        config: {
-          ...sharedControls.x_axis_time_format,
-          default: 'smart_date',
-          description: `${D3_TIME_FORMAT_DOCS}. ${TIME_SERIES_DESCRIPTION_TEXT}`,
-          visibility: ({ controls }: ControlPanelsContainerProps) =>
-            isXAxis ? isVertical(controls) : isHorizontal(controls),
-        },
-      },
-    ],
-    [
-      {
-        name: xAxisLabelRotation.name,
-        config: {
-          ...xAxisLabelRotation.config,
-          visibility: ({ controls }: ControlPanelsContainerProps) =>
-            isXAxis ? isVertical(controls) : isHorizontal(controls),
-        },
-      },
-    ],
-    [
-      {
-        name: 'y_axis_format',
-        config: {
-          ...sharedControls.y_axis_format,
-          label: t('Axis Format'),
-          visibility: ({ controls }: ControlPanelsContainerProps) =>
-            isXAxis ? isHorizontal(controls) : isVertical(controls),
-        },
-      },
-    ],
-    ['currency_format'],
-    [
-      {
-        name: 'logAxis',
-        config: {
-          type: 'CheckboxControl',
-          label: t('Logarithmic axis'),
-          renderTrigger: true,
-          default: logAxis,
-          description: t('Logarithmic axis'),
-          visibility: ({ controls }: ControlPanelsContainerProps) =>
-            isXAxis ? isHorizontal(controls) : isVertical(controls),
-        },
-      },
-    ],
-    [
-      {
-        name: 'minorSplitLine',
-        config: {
-          type: 'CheckboxControl',
-          label: t('Minor Split Line'),
-          renderTrigger: true,
-          default: minorSplitLine,
-          description: t('Draw split lines for minor axis ticks'),
-          visibility: ({ controls }: ControlPanelsContainerProps) =>
-            isXAxis ? isHorizontal(controls) : isVertical(controls),
-        },
-      },
-    ],
-    [
-      {
-        name: 'truncateYAxis',
-        config: {
-          type: 'CheckboxControl',
-          label: t('Truncate Axis'),
-          default: truncateYAxis,
-          renderTrigger: true,
-          description: t('It’s not recommended to truncate axis in Bar chart.'),
-          visibility: ({ controls }: ControlPanelsContainerProps) =>
-            isXAxis ? isHorizontal(controls) : isVertical(controls),
-        },
-      },
-    ],
-    [
-      {
-        name: 'y_axis_bounds',
-        config: {
-          type: 'BoundsControl',
-          label: t('Axis Bounds'),
-          renderTrigger: true,
-          default: yAxisBounds,
-          description: t(
-            'Bounds for the axis. When left empty, the bounds are ' +
-              'dynamically defined based on the min/max of the data. Note that ' +
-              "this feature will only expand the axis range. It won't " +
-              "narrow the data's extent.",
-          ),
-          visibility: ({ controls }: ControlPanelsContainerProps) =>
-            Boolean(controls?.truncateYAxis?.value) &&
-            (isXAxis ? isHorizontal(controls) : isVertical(controls)),
-        },
-      },
-    ],
-  ];
-}
+const { zoomable, orientation } = DEFAULT_FORM_DATA;
 
 const config: ControlPanelConfig = {
   controlPanelSections: [
